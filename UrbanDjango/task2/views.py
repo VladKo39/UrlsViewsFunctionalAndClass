6e49b95467094b070d139f26849bdf363d8d7412
VladKo39 <vladko67@mail.ru>
2025-01-24 13:07:18 +0200

    Add files via upload

---
+++ UrbanDjango/task2/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+# Create your views here.
+
+class class_t(TemplateView):
+    template_name='second_task/class_template.html'
+
+def func_t(request):
+    return render(request,'second_task/func_template.html')
